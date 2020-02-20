471a2caf82162cc93d9497fa908c550b6558e26d
jarrodhBSU <61212737+jarrodhBSU@users.noreply.github.com>
2020-02-20 16:06:51 -0500

    Add files via upload

---
+++ Tutor.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jarrod
+ */
+public class Tutor {
+    
+    private String Name;
+    private String userName;
+    private String password;
+    
+    
+    
+    public Tutor()
+    {
+        
+    }        
+    
+    public Tutor(String nm, String un, String pd)
+    {
+        
+        Name = nm;
+        userName = un;
+        password = pd;
+        
+    }        
+    
+}
