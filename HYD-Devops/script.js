ce90cf7f3b95a68942ea6a4ab071ff1516d250b5
toluwanimi1 <toluwanimi2@gmail.com>
2020-03-24 16:15:11 -0400

    Add files via upload

---
+++ HYD-Devops/script.js
@@ -0,0 +1,13 @@
+// js that keeps the navbar stuck upon scrolling
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
